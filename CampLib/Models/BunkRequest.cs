98b2b2b3b7291d66534265393a7c50d1e4631634
== <cbtesser@gmail.com>
2019-05-26 14:15:59 -0400

    updates

---
+++ CampLib/Models/BunkRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CampLib.Models
+{
+    class BunkRequest
+    {
+        [Key]
+        public int BunkRequestID { get; set; }
+        public string Name { get; set; }
+        public int CamperID { get; set; }
+    }
+}
--- CampLib/Models/Camper.cs
+++ CampLib/Models/Camper.cs
@@ -16,6 +16,16 @@ namespace CampLib.Models
         public Email Email { get; set; }
         public ICollection<Parent> Parents { get; set; }
         public ICollection<EmergencyContact> EmergencyContacts { get; set; }
-
+        public TripAttending TripAttending { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string School { get; set; }
+        public int Grade { get; set; }
+        public string Teacher { get; set; }
+    }
+    public enum TripAttending
+    {
+        First,
+        Second,
+        FullSummer
     }
 }
